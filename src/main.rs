4c1e21d70368ed66e5f630f5976ab23e47b2dff6
Takumi Sato <sata4hobby@gmail.com>
2024-05-30 14:06:10 +0900

    0.2.3 - Add comments & Rewrite usage message

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,65 @@ use anyhow::Result;
 use image::{io::Reader as ImageReader, DynamicImage};
 use jpeg_to_pdf::JpegToPdf;
 use std::{
+  ffi::OsStr,
   fs::{self, File},
   io::{BufWriter, Cursor},
   path::Path,
 };
 
-// Usage: mkpdf <outfile>.pdf <source1>.jpg [<source2>.jpg ...]
+fn print_usage() {
+  eprintln!("Usage: mkpdf <output_file> <input_image1> [<input_image2>...]");
+  eprintln!("  <output_file> : if not *.pdf, automatically append '.pdf'");
+  eprintln!("  <input_image> : Supported formats are [.jpg|.png|.bmp]");
+}
 
 fn main() -> Result<()> {
   let args: Vec<String> = std::env::args().collect();
 
   // Quit if no enough args
   if args.len() < 3 {
-    eprintln!("Usage: mkpdf <outfile> <source1> [<source2>...]");
-    eprintln!("       <outfile> : if not *.pdf, automatically append '.pdf'");
-    eprintln!("       <sources> : Supported formats are [.jpg|.png|.bmp]");
+    print_usage();
     return Ok(());
   }
 
-  let mut out_file_name = args[1].clone();
-  if out_file_name.ends_with(".pdf") {
+  // Get output file basename
+  let output_file_name = if args[1].ends_with(".pdf") {
     // Trim ".pdf" from out file name
-    out_file_name = out_file_name.trim_end_matches(".pdf").to_string();
-  }
+    args[1].trim_end_matches(".pdf").to_string()
+  } else {
+    args[1].clone()
+  };
 
-  let src_images: Vec<String> = args[2..].to_vec();
+  let input_images = &args[2..];
 
   // Create file - Quit if already exist same name file
-  let out_file = File::create_new(out_file_name.clone() + ".pdf")?;
+  let output_file = File::create_new(output_file_name.clone() + ".pdf")?;
 
   // Create pdf object
   let mut pdf = JpegToPdf::new();
 
   // Add images to pdf
-  for src_image_path in src_images {
-    let src_image_path = Path::new(&src_image_path);
+  for input_image_path in input_images {
+    // Convert image path from String to Path
+    let input_image_path = Path::new(&input_image_path);
 
-    let jpg_image;
-    if &*src_image_path.extension().unwrap() == "jpg" {
+    let jpg_image = if &input_image_path.extension() == &Some(OsStr::new("jpg")) {
       // Load image from jpeg file
-      jpg_image = fs::read(src_image_path)?;
+      fs::read(input_image_path)?
     } else {
       // Load image from other than jpeg file
-      let image = ImageReader::open(src_image_path)?.decode()?;
-      jpg_image = convert_to_jpeg(image)?;
-    }
+      let image = ImageReader::open(input_image_path)?.decode()?;
+      convert_to_jpeg(image)?
+    };
+
     pdf = pdf.add_image(jpg_image);
   }
 
   // Export to pdf file
   pdf
     .strip_exif(true)
-    .set_document_title(out_file_name)
-    .create_pdf(&mut BufWriter::new(out_file))?;
+    .set_document_title(output_file_name)
+    .create_pdf(&mut BufWriter::new(output_file))?;
 
   Ok(())
 }
@@ -68,5 +74,6 @@ fn convert_to_jpeg(image: DynamicImage) -> Result<Vec<u8>> {
   image
     .into_rgb8()
     .write_to(&mut seekable_jpg_buf, image::ImageFormat::Jpeg)?;
+
   Ok(jpg_buf)
 }
